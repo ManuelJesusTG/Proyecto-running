c1f1ac3c263ff4423fd3b0c83233946ac09a2f71
ManuelJesusTG <90841523+ManuelJesusTG@users.noreply.github.com>
2023-02-19 21:56:06 +0100

    Funciones del video

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,33 @@
-function DeleteVideo(){
+window.onload = function(){
+    var IniciarPausar = document.getElementById("IniciarPausar")
     var video = document.getElementById("myVideo");
+    var Velocidad = document.getElementById("Velocidad")
+
+
+IniciarPausar.addEventListener("click", function(){
+    if(video.paused){
+        video.play();
+        IniciarPausar.innerHTML = "⏸️"
+    } else{
+        video.pause()
+        IniciarPausar.innerHTML = "▶️"
+    }
+})
+
+video.addEventListener("timeupdate", function(){
+    var tiempo = video.currentTime / video.duration *100;
+    Velocidad.value = tiempo;
+})
+
+Velocidad.addEventListener("change", function(){
+    var cambios = Velocidad.value * video.duration /100;
+    video.currentTime = cambios;
+})
+
+}
+
+function DeleteVideo(){
+    
     var div = document.getElementById("content")
     var animation = document.getElementById("animation")
 
@@ -7,17 +35,3 @@ function DeleteVideo(){
     video.remove();
     div.remove();
 }
-
-
-
-document.getElementById('id-sun').onclick = function(){
-    document.getElementById('page').classList.remove('dark-mode')
-    document.getElementById('id-moon').classList.remove('active')
-    this.classList.add('active')
-  }
-
-  document.getElementById('id-moon').onclick = function(){
-    document.getElementById('page').classList.add('dark-mode')
-    document.getElementById('id-sun').classList.remove('active')
-    this.classList.add('active')
-  }
