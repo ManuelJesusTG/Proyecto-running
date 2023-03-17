6501ab1ff658546de2b66ea95baf186b81d3d1e5
ManuelJesusTG <alum.mjesusterrong@iesalixar.org>
2023-03-17 13:36:40 +0100

    Modo Oscuro por finalizar

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
+// Modo oscuro y modo Claro
+
+window.onload = Comenzar;
+
+function Comenzar() {
+
+    document.getElementById("lightSwitch").addEventListener("click",ModoOscuro)
+}
+
+function ModoOscuro(){
+    let boton = document.getElementById("lightSwitch");
+
+if (boton.checked == false){
+
+    console.log("desactivado")
+
+    document.getElementById("darkheader").id = "header"
+    document.getElementById("nav").className= "navbar-expand-lg navbar-light bg-light"
+    document.getElementById("dark").id = "body"
+    document.getElementsByClassName("col card text-center m-4").id = "hola"
+
+} else {
+
+    console.log("activado")
+
+    document.getElementById("header").id = "darkheader"
+    document.getElementById("nav").className = "navbar-expand-lg navbar-dark bg-dark"
+    document.getElementById("body").id = "dark"
+    document.getElementsByClassName("col card text-center m-4").id = "hola"
+}
+}
+
+
 // Funcion para eliminar el video y acceder a la página
 
 function DeleteVideo(){
@@ -9,6 +42,3 @@ function DeleteVideo(){
     video.remove();
     div.remove();
 }
-
-// Modo oscuro y modo Claro
-
