eccf1bdae5527bc7976b7f52bfa39a13de152693
ManuelJesusTG <alum.mjesusterrong@iesalixar.org>
2023-03-17 10:57:03 +0100

    Controles del video bien implementados

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,4 @@
-window.onload = function(){
-    var IniciarPausar = document.getElementById("IniciarPausar")
-    var video = document.getElementById("myVideo");
-    var Velocidad = document.getElementById("Velocidad")
-
-
-
-    // el video consta de 3 imagenes, con githubpages hay veces que tarda en cargar y no funciona correctamente
-IniciarPausar.addEventListener("click", function(){
-    if(video.paused){
-        video.play();
-        IniciarPausar.innerHTML = "⏸️"
-    } else{
-        video.pause()
-        IniciarPausar.innerHTML = "▶️"
-    }
-})
-
-video.addEventListener("timeupdate", function(){
-    var tiempo = video.currentTime / video.duration *100;
-    Velocidad.value = tiempo;
-})
-
-Velocidad.addEventListener("change", function(){
-    var cambios = Velocidad.value * video.duration /100;
-    video.currentTime = cambios;
-})
-
-}
+// Funcion para eliminar el video y acceder a la página
 
 function DeleteVideo(){
     
