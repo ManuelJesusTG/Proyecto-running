4be8a5d5d9f196d36e2245ae438fa35a471463b8
ManuelJesusTG <alum.mjesusterrong@iesalixar.org>
2023-03-24 12:08:39 +0100

    Cambios en Modo Oscuro

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,10 @@ if (boton.checked == false){
     document.getElementById("darkheader").id = "header"
     document.getElementById("nav").className= "navbar-expand-lg navbar-light bg-light"
     document.getElementById("dark").id = "body"
+    document.getElementById("login").className = "modal-content"
+    for (let i = 0; i < document.getElementsByTagName("a").length; i++) {
+        document.getElementsByTagName("a")[i].id="linkN"
+    }
     document.getElementById("noticia1").className = "col card text-center m-4"
     document.getElementById("noticia2").className = "col card text-center m-4"
     document.getElementById("noticia3").className = "col card text-center m-4"
@@ -29,6 +33,10 @@ if (boton.checked == false){
     document.getElementById("header").id = "darkheader"
     document.getElementById("nav").className = "navbar-expand-lg navbar-dark bg-dark"
     document.getElementById("body").id = "dark"
+    document.getElementById("login").className = "modal-content bg-dark"
+    for (let i = 0; i < document.getElementsByTagName("a").length; i++) {
+        document.getElementsByTagName("a")[i].id="linkO"
+    }
     document.getElementById("noticia1").className = "col card text-center m-4 bg-dark"
     document.getElementById("noticia2").className = "col card text-center m-4 bg-dark"
     document.getElementById("noticia3").className = "col card text-center m-4 bg-dark"
